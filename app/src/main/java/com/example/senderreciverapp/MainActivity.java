15eb93e83c980019f54792f9eb75eb52aab725bf
matankribus <matanwx135@gmail.com>
2021-02-22 15:26:43 +0200

    Version 1.1.b P.O.C peer to peer. Added the ability to decide on string to send and display the current external IP of the device

--- app/src/main/java/com/example/senderreciverapp/MainActivity.java
+++ app/src/main/java/com/example/senderreciverapp/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.senderreciverapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.format.Formatter;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Observable;
 
 public class MainActivity extends AppCompatActivity {
     public SenderReciver senderReciver;//Gets assigned in the bindService call in the onCreate callback method.
+    private TextView recivedElement;
+    private String ip;
+    private TextView ipDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ip = getIP();
         setContentView(R.layout.activity_main);
+
+
+        recivedElement =  (TextView) findViewById(R.id.MessageRecivedDisplay);
+        ipDisplay = (TextView) findViewById(R.id.IPDisplay);
+        ipDisplay.setText(ip);
         Intent intent = new Intent(this,SenderReciver.class);
         ServiceConnection connection = new ServiceConnection() {
             @Override
@@ -33,9 +54,27 @@ public class MainActivity extends AppCompatActivity {
         };
         startService(intent);
         bindService(intent,connection,0);
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                recivedElement.setText(s);
+            }
+        };
+        Log.d("test", "onCreate: Ip is:" + ip);
+
+
     }
-    
+
+    public String getIP(){
+        WifiManager wifiManager = (WifiManager) getApplication().getSystemService(WIFI_SERVICE);
+        String ipAddress = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
+        return ipAddress;
+    }
     public void buttonPressed(View view) {
-        senderReciver.sendData("127.0.0.1","Hi!\n");
+        senderReciver.sendData(
+                ((TextView) this.findViewById(R.id.TextInputIP)).getText().toString(),
+                ((TextView) this.findViewById(R.id.TextInputMessage)).getText().toString());
     }
+
 }
--- app/src/main/java/com/example/senderreciverapp/SenderReciver.java
+++ app/src/main/java/com/example/senderreciverapp/SenderReciver.java
@@ -3,12 +3,16 @@ package com.example.senderreciverapp;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.LongDef;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -27,6 +31,7 @@ public class SenderReciver extends Service {
     static boolean senderRuning;
     static private Socket socket;
     final ServerSocket listener = new ServerSocket(7000);
+    static MutableLiveData<String> recived = new MutableLiveData<>();
 
     private final IBinder binder = new LocalBinder();
     /**
@@ -37,6 +42,8 @@ public class SenderReciver extends Service {
             return SenderReciver.this;
         }
     }
+
+
 
 
     static class Socketed implements AutoCloseable, EventListener {
@@ -114,7 +121,8 @@ public class SenderReciver extends Service {
                     try (Socketed socketed = new Socketed(serverSocket.accept())) {//Blocks until the socket receives a connection.
                         BufferedReader reader = socketed.getReader();
                         String line = reader.readLine();
-                        Log.d("senderReceiver,Receiver", "startReceiver: received ->\n" + line);
+                        //Log.d("senderReceiver,Receiver", "startReceiver: received ->\n" + line);
+                        recived.setValue(line);
                         socketed.send("ack");
                     } catch (Exception e) {
                         e.printStackTrace();
